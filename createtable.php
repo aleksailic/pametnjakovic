ae431b4cda6c58ad709d18307114bd62df2af155
Aleksa Ilic <aleksa.d.ilic@gmail.com>
2013-09-29 14:08:18 +0200

    Error in createtable php Fix #2

--- createtable.php
+++ createtable.php
@@ -1,12 +1,14 @@
 <?php
 	include('includes/connect.php');
-	echo 'IMMA GOIN TO TRY TO UPDATE DB!';
-	pg_query($con,"CREATE TABLE films (
-    code        char(5) CONSTRAINT firstkey PRIMARY KEY,
-    title       varchar(40) NOT NULL,
-    did         integer NOT NULL,
-    date_prod   date,
-    kind        varchar(10),
-    len         interval hour to minute
-);") or die("DAFUQ HAPPENED!");
+	echo 'IMMA GOIN TO TRY TO UPDATE DB! AGAIN!';
+	pg_query($con,"CREATE TABLE scores (
+  id int(11) NOT NULL serial PRIMARY KEY,
+  fbid text CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+  time int(11) NOT NULL,
+  maxscore int(11) NOT NULL,
+  score int(11) NOT NULL,
+  games int(11) NOT NULL,
+  achievments text NOT NULL,
+);") or die("WHY DONT YOU WORK!");
+
 ?>
