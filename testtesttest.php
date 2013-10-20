02e94253ae55875bcdec5f9f52f5b98348731c23
Aleksa Ilic <aleksa.d.ilic@gmail.com>
2013-10-20 23:38:22 +0200

    I'm sick of this (Fix#1 for testtesttest.php)

--- testtesttest.php
+++ testtesttest.php
@@ -1,3 +1,11 @@
 <?php
-	echo "<B>IM A BARBIE GIRL</B>";
+	$host='http://sql4.freesqldatabase.com';
+$db='sql420761';
+$user='sql420761';
+$pass='bW5!fQ3%';
+// Create connection
+$con = new mysqli($host, $user, $pass, $db);
+   if($mysqli->connect_error) {
+     die('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
+   }
 ?>
